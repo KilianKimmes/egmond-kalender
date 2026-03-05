94fa7410fbf4b69834d3df0d34809aa7242dc050
Egmond <kilian.kimmeskamp@googlemail.com>
2026-03-05 16:57:15 +0100

    ok

--- README.txt
+++
@@ -1,2 +0,0 @@
-Halbtag-Fix + Overlap-Blocker: saveBooking() prüft jetzt Verfügbarkeit pro Tag (inkl. Halbtage) und verhindert Überschneidungen.
-Deploy: ersetze index.html/manifest.json/sw.js im Repo /kalender. Danach Hard-Reload bzw. Service Worker Cache löschen.
--- sw.js
+++ sw.js
@@ -1,23 +1,4 @@
-const CACHE='egmond-v3';
-const ASSETS=['./','./index.html','./manifest.json','./sw.js','./images/wappen_neu.png'];
-self.addEventListener('install',e=>{
- e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
- self.skipWaiting();
-});
-self.addEventListener('activate',e=>{
- e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>x!==CACHE?caches.delete(x):null))));
- self.clients.claim();
-});
-self.addEventListener('fetch',e=>{
- const r=e.request;
- if(r.method!=='GET')return;
- const u=new URL(r.url);
- if(u.origin!==location.origin)return;
- e.respondWith(
-  caches.match(r).then(res=>res||fetch(r).then(net=>{
-   const copy=net.clone();
-   caches.open(CACHE).then(c=>c.put(r,copy));
-   return net;
-  }))
- );
-});
+// Minimal Service Worker für PWA-Installation
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
